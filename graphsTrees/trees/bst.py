3c07c14545e5a9aa2b20562e04f0676f978123fc
Aditya Saluja <adisal97@student.ubc.ca>
2020-12-03 19:25:49 -0800

    seach method created for BST

--- graphsTrees/trees/bst.py
+++ graphsTrees/trees/bst.py
@@ -1,1 +1,14 @@
+from node import Node
+from tree import Tree
 
+class BST(Tree):
+    
+    def search_node(self,node,key):
+        if node is None:
+            return False
+        if node.key == key:
+            return True
+        if node.key<key:
+            return self.search_node(node.right,key)
+        else:
+            return self.search_node(node.left,key)
